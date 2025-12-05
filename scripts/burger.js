e8e97de0bd902ffbcc3e7ab4b50eab384c1a3b7f
gewarteter <ankakotik74@gmail.com>
2025-12-05 19:16:24 +0400

    Delete scripts directory

--- scripts/burger.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("burger_menu").addEventListener("click", function(){
-        document.querySelector(".navigation").classList.toggle("open")
-    })
-})
